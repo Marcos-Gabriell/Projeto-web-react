0a5c449b4bab0c892bfcd3f756faa93e4d5f4e4f
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-06-23 22:33:14 -0300

    teste novamente

--- src/MySection3.js
+++ src/MySection3.js
@@ -4,26 +4,26 @@ import logoImg from './img/aten.png';
 
 function MySection3() {
   return (
-    <div className="info-image">
+    <div className="info-image"><br/>
       <img src={logoImg} alt="Imagem de informações" />
       <div className="info-overlay">
         <div className="info-box">
-          <h4>ALGUNS FATOS SOBRE O NOSSO TRABALHO:</h4>
+          <h4><br/><br/>ALGUNS FATOS SOBRE O NOSSO TRABALHO:</h4>
           <div className="fact-container">
             <div className="fact">
-              <span className="fact-number">752</span><br/><br/>
+              <span className="fact-number">752</span><br/>
               <span className="fact-label">CLIENTES ATENDIDOS</span>
             </div>
             <div className="fact">
-              <span className="fact-number">17</span><br/><br/>
+              <span className="fact-number">17</span><br/>
               <span className="fact-label">ESTADOS ATENDIDOS</span>
             </div>
             <div className="fact">
-              <span className="fact-number">3500</span><br/><br/>
+              <span className="fact-number">3500</span><br/>
               <span className="fact-label">PROJETOS ANALISADOS</span>
             </div>
             <div className="fact">
-              <span className="fact-number">16</span><br/><br/>
+              <span className="fact-number">16</span><br/>
               <span className="fact-label">SETORES ATENDIDOS</span>
             </div>
           </div>
