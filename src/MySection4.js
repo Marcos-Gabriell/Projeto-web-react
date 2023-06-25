369fc780dcbd31bc050198113d6e4952ddc3e9c9
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-06-25 12:32:32 -0300

    ajuste nas fontes

--- src/MySection4.js
+++ src/MySection4.js
@@ -4,10 +4,10 @@ import './MySection4.css';
 const QuemSomos = () => {
   return (
     <section className="quemsomos">
-      <h1>A experiência da Rocha Marques<br/> oferece às empresas a elaboração <br/>de um processo assertivo e com<br/> qualidade.</h1><br/>
+      <h1>A experiência da Rocha Marques<br/> oferece às empresas a elaboração <br/>de um processo assertivo e com<br/> qualidade.</h1>
       <div>
         <h2>QUEM SOMOS.</h2>
-        <h3>A Rocha Marques  foi fundada em Curitiba em 1995 com <br/>o objetivo de assessorar empresas de informática e <br/>automação de todos os portes a se habilitar e a manter <br/>os incentivos fiscais da Lei de Informática.</h3>
+        <h3>A Rocha Marques  foi fundada em Curitiba em 1995 com <br/>o objetivo de assessorar empresas de informática e <br/>automação de todos os portes a se habilitar e a manter <br/>os incentivos fiscais da Lei de Informática.</h3><br/>
         <button className="saiba-mais-btn">Leia mais </button>
       </div>
     </section>
