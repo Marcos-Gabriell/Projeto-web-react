5a3419ec341ca815051d459cb8b5c407a68f8ebc
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-06-30 22:39:37 -0300

    ajustes e teste do responsvio na pagina leidainformatica

--- src/leidainformatica.js
+++ src/leidainformatica.js
@@ -24,38 +24,39 @@ function leidainformatica() {
         </div>
         <SeparatorLine />
       
-          <div className="containerr1">
-            <h1>Empresas fabricantes de equipamentos de informática, comunicação e automação podem pleitear créditos financeiros através investimentos em pesquisa, desenvolvimento e inovação – P,D&I.</h1>
-            <div className="text-containerr1">
-            <h2>Quem pode se beneficiar?</h2>
-            <p>Empresas brasileiras de qualquer porte que fabriquem localmente bens de informática e <br/>
-            automação, conforme regulamentado em Decreto. Entre em contato conosco e nós <br/> 
-            faremos esta avaliação para a sua empresa para o processo de gestão da Lei de <br/>
-            Informática.</p>
+        <div className="coontainerr1">
+            <h1>Empresas estabelecidas na Amazônia Ocidental ou no Amapá e fabricantes de equipamentos de informática, <br/>
+            comunicação e automação que investem em pesquisa, desenvolvimento e inovação – P,D&I têm isenção de IPI <br/>
+            e redução de II na compra de insumos.</h1><br/><br/><br/>
+            <div className="texxt-containerr1">
+            <h2>Quem pode se beneficiar da Lei?</h2>
+            <p>Empresas brasileiras de qualquer porte que fabriquem Amazônia Ocidental ou no Estado do <br/>
+             Amapá bens de informática e automação conforme regulamentado em Decreto. Entre em <br/>
+             contato conosco e nós faremos esta avaliação para a sua empresa em busca da <br/>
+             conformidade da Lei de Informática na Zona Franca de Manaus.</p>
         </div>
-        <div className="text-containerr1">
+        <div className="texxt-containerr1">
             <h2>Qual o benefício para a sua empresa?</h2>
-            <p>Crédito financeiro a ser compensado com tributos federais (IPI, IRPJ, CSLL, PIS, COFINS) em <br/>
-            até 5 anos.</p>
+            <p>Isenção do IPI – Imposto sobre Produtos Industrializados, quando da venda dos produtos <br/>
+            incentivados e redução do II – Imposto de importação na compra de insumos para fabricação <br/>
+             dos produtos incentivados.</p>
         </div>
-        <div className="text-containerr1">
+        <div className="texxt-containerr1">
             <h2>Como atuamos?</h2>
-            <p>A Rocha Marques presta consultoria em todas as etapas do processo de gestão da Lei de <br />
-            Informática, abrangendo o suporte ao processo de auditoria independente, quando <br/>
-            aplicável.</p>
-        </div>
-        <div className="text-containerr1">
-            <h2>Se a sua empresa não está habilitada, nós elaboramos o pleito de PPB.</h2>
-            <p>Se a sua empresa está habilitada, nós gerimos todas as obrigações oriundas da Lei, desde <br/>
-            a inclusão de um produto ou modelo, da orientação para a geração do crédito financeiro,<br/>
-            da análise de enquadrabilidade dos projetos de P,D&I, até a elaboração do RDA – Relatório <rb/>
-            Demonstrativo da Lei de Informática.</p>
+            <p>A SIGPLANI suporta a empresa na manutenção dos incentivos fiscais da Lei de Informática <br/>
+             na Zona Franca de Manaus, atuando na busca da conformidade no cumprimento das obrigações <br/>
+              de investimento em P,D&I.<br/><br/>
+               Se asua empresa está habilitada na SUFRAMA, nós gerimos todas as obrigações oriundas da Lei, <br/>
+               desde a inclusão de um produto ou modelo, da análise de enquadrabilidade dos projetos de <br/>
+               P,D&I, até a prestação de contas.</p>
         </div>   
-        <div className="image-containerr1">
-        <img src={logoImg3} alt="Imagem" />
-        </div> 
-        <h4>São mais de 25 anos de experiência na gestão da Lei de Informática à disposição para que este incentivo seja estratégico na sua empresa!</h4>
       </div>
+         <div className="imagee-containerr1">
+         <img src={logoImg3} alt="Imagem" />
+        </div>
+        <div className="subtitulo">
+           <h4>São mais de 25 anos de experiência na gestão da Lei de Informática à disposição para que este incentivo seja estratégico na sua empresa!</h4>
+        </div>
       <div className="infoo-imagee3">
       <img src={logoImg4} alt="Imagem" />
       </div>
