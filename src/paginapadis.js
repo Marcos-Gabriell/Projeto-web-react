094da71c3483442e0fa72427abbd57e4799978cf
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-07-02 17:05:01 -0300

    Salvar alterações no arquivo paginapadis.js

--- src/paginapadis.js
+++ src/paginapadis.js
@@ -13,10 +13,47 @@ function paginapadis() {
     <img src={logoImg} alt="Imagem" style={{ objectFit: 'cover' }} /> 
     <div className="ooverlayy8">
     <p className="imagee-texxtt8">INCENTIVOS FISCAIS</p>
-     <h1>Lei de Informática – Manaus</h1>
+     <h1>PADIS</h1>
    </div>
    <br/> 
    </div>
+
+   <div className="coontainerr1">
+            <h1>Conjunto de incentivos fiscais federais destinado às áreas de componentes ou dispositivos semicondutores, <br/>
+            incluindo células e módulos/painéis fotovoltaicos, insumos e equipamentos dedicados e destinados à <br/>
+             fabricação de componentes ou dispositivos eletrônicos semicondutores, bem como displays (mostradores de informação).</h1><br/><br/><br/>
+            <div className="texxt-containerr1">
+            <h2>Quem pode se beneficiar?</h2>
+            <p>Empresas brasileiras de qualquer porte que fabriquem localmente bens de informática e <br/>
+            automação, conforme regulamentado em Decreto. Entre em contato conosco e nós <br/>
+            faremos esta avaliação para a sua empresa para o processo de gestão da Lei de <br/>
+             Informática.</p>
+        </div>
+        <div className="texxt-containerr1">
+            <h2>Qual o benefício para a sua empresa?</h2>
+            <p>Crédito financeiro a ser compensado com tributos federais (IPI, IRPJ, CSLL, PIS, COFINS) em <br/>
+              até 5 anos.</p>
+        </div>
+        <div className="texxt-containerr1">
+            <h2>Como atuamos?</h2>
+            <p>A SIGPLANI presta consultoria em todas as etapas do processo de gestão da Lei de <br/>
+            Informática, abrangendo o suporte ao processo de auditoria independente, quando <br/>
+            aplicável.</p>
+        </div>   
+        <div className="texxt-containerr1">
+            <h2>Se a sua empresa não está habilitada, nós elaboramos o pleito de PPB.</h2>
+            <p>Se a sua empresa está habilitada, nós gerimos todas as obrigações oriundas da Lei, desde <br/>
+              a inclusão de um produto ou modelo, da orientação para a geração do crédito financeiro, <br/>
+              da análise de enquadrabilidade dos projetos de P,D&I, até a elaboração do RDA – Relatório <br/>
+              Demonstrativo da Lei de Informática.</p>
+        </div>   
+      </div>
+         <div className="imagee-containerr1">
+         <img src={logoImg1} alt="Imagem" />
+        </div>
+        <div className="subtitulo">
+           <h4>São mais de 25 anos de experiência na gestão da Lei de Informática à disposição para que este incentivo seja estratégico na sua empresa!</h4>
+        </div>
   <MyCadastro />
   <End />
   <Myrodape />
