7ee2e08ac4dea9de723f488af441b4325238e667
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-07-04 11:11:42 -0300

    teste do responsivo da pagina de conatato

--- src/MySection4.js
+++ src/MySection4.js
@@ -6,7 +6,7 @@ const QuemSomos = () => {
   return (
     <section className="quemsomos">
       <h1>A experiência da SIGPLANI<br/> oferece às empresas a elaboração <br/>de um processo assertivo e com<br/> qualidade.</h1>
-      <div>
+      <div className="quemsomos2">
         <h2>QUEM SOMOS.</h2>
         <h3>A SIGPLANI  foi fundada em Curitiba em 1995 com <br/>o objetivo de assessorar empresas de informática e <br/>automação de todos os portes a se habilitar e a manter <br/>os incentivos fiscais da Lei de Informática.</h3><br/>
         <Link to="/a-sigplani/" className="btn">Saiba Mais</Link>
